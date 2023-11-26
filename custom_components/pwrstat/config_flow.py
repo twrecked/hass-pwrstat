2c3f525ca9fc763454b8fee3f97fe8b0be5e3475
Steve Herrell <steve.herrell@gmail.com>
2023-11-25 23:48:56 -0500

    Add some error handling for config flow.

--- custom_components/pwrstat/config_flow.py
+++ custom_components/pwrstat/config_flow.py
@@ -8,7 +8,10 @@ import logging
 import voluptuous as vol
 from typing import Any
 
-from homeassistant import config_entries, exceptions
+from homeassistant import (
+    config_entries,
+    exceptions
+)
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import *
@@ -21,16 +24,25 @@ class PwrStatConfigFlow(config_entries.ConfigFlow, domain=COMPONENT_DOMAIN):
 
     VERSION = 1
 
+    async def validate_input(self, user_input):
+        for ups_name, values in self.hass.data.get(COMPONENT_DOMAIN, {}).items():
+            _LOGGER.debug(f"checking {ups_name}")
+            if ups_name == user_input[ATTR_UPS_NAME]:
+                raise UPSNameAlreadyUsed
+
     async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
 
         errors = {}
-
-        if user_input is not None:
-            return self.async_create_entry(title=user_input[ATTR_UPS_NAME], data={
-                ATTR_UPS_NAME: user_input[ATTR_UPS_NAME],
-                ATTR_UPS_URL: user_input[ATTR_UPS_URL],
-                ATTR_UPS_POLL_EVERY: user_input[ATTR_UPS_POLL_EVERY],
-            })
+        try:
+            if user_input is not None:
+                await self.validate_input(user_input)
+                return self.async_create_entry(title=user_input[ATTR_UPS_NAME], data={
+                    ATTR_UPS_NAME: user_input[ATTR_UPS_NAME],
+                    ATTR_UPS_URL: user_input[ATTR_UPS_URL],
+                    ATTR_UPS_POLL_EVERY: user_input[ATTR_UPS_POLL_EVERY],
+                })
+        except UPSNameAlreadyUsed as _e:
+            errors["base"] = "name_used"
 
         # Fill in defaults if we have nothing.
         if user_input is None:
@@ -51,8 +63,4 @@ class PwrStatConfigFlow(config_entries.ConfigFlow, domain=COMPONENT_DOMAIN):
 
 
 class UPSNameAlreadyUsed(exceptions.HomeAssistantError):
-    """ Error indicating group name already used. """
-
-
-class UPSURLAlreadyUsed(exceptions.HomeAssistantError):
     """ Error indicating group name already used. """
